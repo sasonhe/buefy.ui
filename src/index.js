f3396d900093fc11c6debd53cf424d7d9f17f395
sasonhe <642139009@qq.com>
2017-10-06 19:13:10 +0800

    add

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
 // 引入路由配置
 import routes from './router.config'
-//全局样式重置
-import reset from './common/reset.css'
 // 注册路由
 Vue.use(VueRouter)
 Vue.use(VueResource)
+Vue.use(Buefy)
 
 var router = new VueRouter({
     routes
