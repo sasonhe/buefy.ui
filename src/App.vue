510b56c9414966cf3ac742b0b464e4a2ef511e1f
sasonhe <642139009@qq.com>
2017-10-07 09:03:56 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
+    <div class="container">
         <hello></hello>
+        <!-- <modle></modle> -->
     </div>
 </template>
 <script>
 import hello from './components/hello.vue'
+// import modle from './components/modle.vue'
 export default{
     components:{
         hello
+        // modle
     }
 }
 </script>
